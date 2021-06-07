4e61cb206dc0d84993058e4717756b0b4ebe55be
Oliver <oliver@Olivers-MacBook-Pro.local>
2021-06-07 12:36:16 +0200

    Added new endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Express Demo App Test</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-app.get('/products', (req, res) => {
+app.get('/productsTest', (req, res) => {
   res.send([
     {
       productId: '101',
@@ -16,6 +16,34 @@ app.get('/products', (req, res) => {
       productId: '102',
       price: 150
     }
+  ])
+})
+
+app.get('/products', (req, res) => {
+  res.send([     
+  {
+    "id": 1,
+    "title": "Getting started",
+    "description": "Empower your NuxtJS application with @nuxt/content module: write in a content/ directory and fetch your Markdown, JSON, YAML and CSV files through a MongoDB like API, acting as a Git-based Headless CMS.",
+    "heroImg": "",
+    "img": "https://hellolive.tv/img/img_meta.png"
+  },
+  {
+    "id": 2,
+    "title": "My first Blog Post",
+    "description": "Learning how to use @nuxt/content to create a blog",
+    "img": "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    "heroImg": "",
+    "alt": "my first blog post"
+  },
+  {
+    "id": 3,
+    "title": "My second Blog Post",
+    "description": "Learning how to use @nuxt/content to create a blog",
+    "img": "https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg",
+    "heroImg": "",
+    "alt": "my second blog post"
+  } 
   ])
 })
 
